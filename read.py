1c083091d2eb6ff9af8f9a45d56f8b532fec644f
ShenQ <gp3g4xup6@gmail.com>
2022-04-17 18:23:36 +0800

    calc

--- read.py
+++ read.py
@@ -4,11 +4,14 @@ with open('C:/Users/Shen/Desktop/reviews.txt', 'r') as file:
     for line in file:
         data.append(line)
         count += 1 
-        if count % 1000 == 0:  #每一千筆  回傳一次進度  % = 取餘數 == 0
+        if count % 1000000 == 0:  #每一千筆  回傳一次進度  % = 取餘數 == 0
             print(len(data))
-print(len(data))
 
-print(data[0])
-print('-------------------------------------'*3)
-print(data[1])
- 
+# print('檔案讀取完了,總共有', len(data), '筆資料')
+
+
+sum_len = 0
+for d in data:
+    sum_len += len(d)
+
+print('留言的平均長度是', sum_len / len(data))
